be7297f57ad1534e6f7fa7b8c06980b29bfc76fe
Go Sagawa <drunkturtle@gmail.com>
2018-12-24 13:56:38 +0900

    add color

--- xmastree/xmastree.go
+++ xmastree/xmastree.go
@@ -3,23 +3,23 @@ package xmastree
 import "fmt"
 
 const (
-	EscapeRed   = "\033[31m"
-	EscapeGreen = "\033[32m"
-	EscapeBlue  = "\033[33m"
-	EscapeReset = "\033[0m"
+	EscapeRed    = "\033[31m"
+	EscapeGreen  = "\033[32m"
+	EscapeYellow = "\033[33m"
+	EscapeReset  = "\033[0m"
 )
 
 func Display() {
-	fmt.Println(EscapeGreen)
+	fmt.Print(EscapeGreen)
 	fmt.Println("    ¥    ")
 	fmt.Println("   ¥¥¥   ")
 	fmt.Println("  ¥¥¥¥¥  ")
 	fmt.Println(" ¥¥¥¥¥¥¥ ")
-	fmt.Println(EscapeRed)
+	fmt.Print(EscapeRed)
 	fmt.Println("   [ ]   ")
 	fmt.Println("   [ ]   ")
 	fmt.Println("   ```   ")
-	fmt.Println(EscapeBlue)
+	fmt.Print(EscapeYellow)
 	fmt.Println("Merry, Christmas !!!")
 	fmt.Println(EscapeReset)
 
