b0f6a05953625c8606b22da4bb239449dcf26c49
Go Sagawa <drunkturtle@gmail.com>
2018-12-24 13:54:54 +0900

    display tree

--- xmastree/xmastree.go
+++ xmastree/xmastree.go
@@ -2,13 +2,25 @@ package xmastree
 
 import "fmt"
 
+const (
+	EscapeRed   = "\033[31m"
+	EscapeGreen = "\033[32m"
+	EscapeBlue  = "\033[33m"
+	EscapeReset = "\033[0m"
+)
+
 func Display() {
+	fmt.Println(EscapeGreen)
 	fmt.Println("    ¥    ")
 	fmt.Println("   ¥¥¥   ")
 	fmt.Println("  ¥¥¥¥¥  ")
 	fmt.Println(" ¥¥¥¥¥¥¥ ")
+	fmt.Println(EscapeRed)
 	fmt.Println("   [ ]   ")
 	fmt.Println("   [ ]   ")
 	fmt.Println("   ```   ")
+	fmt.Println(EscapeBlue)
 	fmt.Println("Merry, Christmas !!!")
+	fmt.Println(EscapeReset)
+
 }
