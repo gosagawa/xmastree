cb75c63097db6e80256dc09e794c8155e17265f7
Go Sagawa <drunkturtle@gmail.com>
2018-12-24 14:33:42 +0900

    display by logic

--- xmastree/xmastree.go
+++ xmastree/xmastree.go
@@ -9,18 +9,65 @@ const (
 	EscapeReset  = "\033[0m"
 )
 
+type Tree struct {
+	MaxWidth   int
+	LeafHeight int
+	StemWidth  int
+	StemHeight int
+}
+
 func Display() {
+	tree := newTree(5, 1, 2)
+	tree.display()
+
+}
+
+func newTree(leafHeight int, stemWidth int, stemHeight int) *Tree {
+	tree := Tree{
+		LeafHeight: leafHeight,
+		StemWidth:  stemWidth,
+		StemHeight: stemHeight,
+	}
+
+	tree.MaxWidth = tree.LeafHeight*2 + 1
+	return &tree
+}
+
+func (t *Tree) display() {
+
 	fmt.Print(EscapeGreen)
-	fmt.Println("    ¥    ")
-	fmt.Println("   ¥¥¥   ")
-	fmt.Println("  ¥¥¥¥¥  ")
-	fmt.Println(" ¥¥¥¥¥¥¥ ")
+	for i := 0; i < t.LeafHeight; i++ {
+		leafAmount := i*2 + 1
+		spaceLength := (t.MaxWidth - leafAmount) / 2
+		echoString(" ", spaceLength)
+		echoString("¥", leafAmount)
+		echoString(" ", spaceLength)
+		fmt.Println("")
+	}
+
 	fmt.Print(EscapeRed)
-	fmt.Println("   [ ]   ")
-	fmt.Println("   [ ]   ")
-	fmt.Println("   ```   ")
+	for i := 0; i < t.StemHeight; i++ {
+		spaceLength := (t.MaxWidth - t.StemWidth - 2) / 2
+		echoString(" ", spaceLength)
+		echoString("[", 1)
+		echoString(" ", t.StemWidth)
+		echoString("]", 1)
+		echoString(" ", spaceLength)
+		fmt.Println("")
+	}
+	spaceLength := (t.MaxWidth - t.StemWidth - 2) / 2
+	echoString(" ", spaceLength)
+	echoString("`", t.StemWidth+2)
+	echoString(" ", spaceLength)
+	fmt.Println("")
+
 	fmt.Print(EscapeYellow)
 	fmt.Println("Merry, Christmas !!!")
 	fmt.Println(EscapeReset)
+}
 
+func echoString(str string, amount int) {
+	for i := 0; i < amount; i++ {
+		fmt.Print(str)
+	}
 }
